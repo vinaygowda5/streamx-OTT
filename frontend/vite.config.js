ed1f97bba51777d7ac374ce7fefec8c9e78b92de
Vinay <vinaygowda12096909@gmail.com>
2026-05-25 14:28:33 +0530

    Fix vite config for Vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    rollupOptions: {
+      input: './index.html'
+    }
+  },
+  server: {
+    port: 5173
   }
 })
